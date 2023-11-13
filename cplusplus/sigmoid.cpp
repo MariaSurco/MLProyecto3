59ad27388d166dd0169144e0fe6b877afba19646
DiegoGCh <64449084+DiegoGCh@users.noreply.github.com>
2023-11-13 00:25:01 -0500

    Add files via upload

--- cplusplus/sigmoid.cpp
+++ cplusplus/sigmoid.cpp
@@ -7,10 +7,17 @@
 #include "ML.h"
 using namespace std;
 
-
-
 int main() {
-    // leer el trina y test
+    std::string capas_ocultas, neuronas_por_capa, num_features;
+    std::getline(std::cin, capas_ocultas);
+    std::getline(std::cin, neuronas_por_capa);
+    std::getline(std::cin, num_features);
+
+    int capas_ocultas_int = std::stoi(capas_ocultas);
+    int neuronas_por_capa_int = std::stoi(neuronas_por_capa);
+    int num_features_int = std::stoi(num_features);
+
+    // leer el train y test
     const char* x_traininfo = "../data_train/x_train.txt";
     vector<vector<double>> x_train;
     leer_x(x_traininfo,x_train);
@@ -23,39 +30,15 @@ int main() {
     leer_x(x_testinfo,x_test);
 
     // caracteristica, numero de capas ocultas, numero de neuronas por capa
-    PerceptronMultilayer nn(10,1,3,1,"sigmoid");
+    PerceptronMultilayer nn(num_features_int,capas_ocultas_int,neuronas_por_capa_int,1,"sigmoid");
     nn.train(x_train,y_train,0.0001,1000);
-    vector<vector<double>> predictions1_3= nn.predict(x_test);
-    const char* archivoSalida1 = "../output_sigmoid/predictions1_3.txt";
-    guardar(archivoSalida1,predictions1_3);
+    vector<vector<double>> predictions= nn.predict(x_test);
+    std::stringstream ss;
+    ss << "../output_sigmoid/predictions" << capas_ocultas << "_" << neuronas_por_capa << ".txt";
+    std::string archivoSalida1 = ss.str();
+    const char* filePath = archivoSalida1.c_str();
 
-
-    PerceptronMultilayer nn2(10,1,2,1,"sigmoid");
-    nn2.train(x_train,y_train,0.0001,1000);
-    vector<vector<double>> predictions1_2= nn2.predict(x_test);
-    const char* archivoSalida2 = "../output_sigmoid/predictions1_2.txt";
-    guardar(archivoSalida2,predictions1_2);
-
-    PerceptronMultilayer nn3(10,3,6,1,"sigmoid");
-    nn3.train(x_train,y_train,0.0001,1000);
-    vector<vector<double>> predictions3_6= nn3.predict(x_test);
-    const char* archivoSalida3 = "../output_sigmoid/predictions3_6.txt";
-    guardar(archivoSalida3,predictions3_6);
-
-    PerceptronMultilayer nn4(10,4,6,1,"sigmoid");
-    nn4.train(x_train,y_train,0.0001,1000);
-    vector<vector<double>> predictions4_6= nn4.predict(x_test);
-    const char* archivoSalida4 = "../output_sigmoid/predictions4_6.txt";
-    guardar(archivoSalida4,predictions4_6);
-
-    PerceptronMultilayer nn5(10,10,8,1,"sigmoid");
-    nn5.train(x_train,y_train,0.0001,1000);
-    vector<vector<double>> predictions10_8= nn5.predict(x_test);
-    const char* archivoSalida5 = "../output_sigmoid/predictions10_8.txt";
-    guardar(archivoSalida5,predictions10_8);
-
-
-
+    guardar(filePath,predictions);
     
     return 0;
 }
